c3a7af24c8e1fff07048ea8a95203bb0e1931186
Davide <davidereale12@gmail.com>
2024-09-17 14:22:12 +0200

    con docker file e compose funzionantne

---
+++ src/main/java/com/javatechie/kafka_docker_installation/KafkaDockerInstallationApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.kafka_docker_installation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDockerInstallationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDockerInstallationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/kafka_docker_installation/KafkaDockerInstallationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.kafka_docker_installation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDockerInstallationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
